40ea353a13631cd068ec9e6fb34ba40715dbd221
Dmitry <kurilenko907@gmail.com>
2024-03-06 11:57:49 +0300

    resources

--- src/test/java/ru/netology/monthtest/services/calcMonthTest.java
+++ src/test/java/ru/netology/monthtest/services/calcMonthTest.java
@@ -2,14 +2,14 @@ package ru.netology.monthtest.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class calcMonthTest {
     @ParameterizedTest
-    @CsvSource({
-            "3,10000,3000,20000",
-            "2,100000,60000,150000"
-    })
+    @CsvFileSource(files = "src/test/resources/monthtest.csv"
+
+    )
     public void test(int expected, int income, int expenses, int threshold) {
 
 
