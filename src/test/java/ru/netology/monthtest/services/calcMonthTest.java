14dab9d9bf5d8bd4ceb4ec6c911bbfc8f22d8e65
Dmitry <kurilenko907@gmail.com>
2024-03-06 11:42:38 +0300

    initiall commit

---
+++ src/test/java/ru/netology/monthtest/services/calcMonthTest.java
@@ -0,0 +1,21 @@
+package ru.netology.monthtest.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class calcMonthTest {
+    @ParameterizedTest
+    @CsvSource({
+            "3,10000,3000,20000",
+            "2,100000,60000,150000"
+    })
+    public void test(int expected, int income, int expenses, int threshold) {
+
+
+        calcMonth service = new calcMonth();
+        int actual = service.calculate(income, expenses, threshold);
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
